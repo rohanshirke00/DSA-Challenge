f9dd7f798b5a649a4da50329169ac4afa103b754
Rohan Shirke <96473300+rohanshirke00@users.noreply.github.com>
2022-07-26 20:13:36 +0530

    Time: 1 ms (63.12%), Space: 46.2 MB (12.27%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.java
+++ 1470-shuffle-the-array/1470-shuffle-the-array.java
@@ -5,28 +5,28 @@ class Solution {
         
         //time & space complexity is O(n)
         
-        // int[] ans = new int[2*n];
-        // int j=0;
-        // for(int i=0; i<2*n; i+=2,j++){
-        //     ans[i] = nums[j];
-        //     ans[i+1] = nums[j+n];
-        // }
-        // return ans;
+        int[] ans = new int[2*n];
+        int j=0;
+        for(int i=0; i<2*n; i+=2,j++){
+            ans[i] = nums[j];
+            ans[i+1] = nums[j+n];
+        }
+        return ans;
         
         
         
         //optimise approach to make space complexity to O(1)
-        for(int i=0; i<n; i++){
-            nums[i+n] = nums[i] * 10000 + nums[i+n];
+//         for(int i=0; i<n; i++){
+//             nums[i+n] = nums[i] * 10000 + nums[i+n];
             
-        }
+//         }
 
-        for(int i=0; i<n; i++){
-            nums[2*i] = nums[i+n]/10000;
-            nums[2*i+1] = nums[i+n]%10000;
+//         for(int i=0; i<n; i++){
+//             nums[2*i] = nums[i+n]/10000;
+//             nums[2*i+1] = nums[i+n]%10000;
             
-        }
-        return nums;
+//         }
+//         return nums;
         
         
         
