8dd53ebc54b7dccc0b4081e89618f9b5f742a487
Rohan Shirke <96473300+rohanshirke00@users.noreply.github.com>
2022-07-26 11:46:12 +0530

    Update 1920-build-array-from-permutation.java
    space complexity is reduced to O(1) from O(n)

--- 1920-build-array-from-permutation/1920-build-array-from-permutation.java
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.java
@@ -3,8 +3,12 @@ class Solution {
     public int[] buildArray(int[] nums) {
         
         int n = nums.length;
-        // int ans[] = new int[n];
-
+//         int ans[] = new int[n];
+//         for(int i=0; i<n; i++){
+//             ans[i] = nums[nums[i]];
+//         }
+//         return ans;
+        
         for(int i=0; i<n; i++){
             nums[i] = n * (nums[nums[i]] % n) + nums[i];
         }
