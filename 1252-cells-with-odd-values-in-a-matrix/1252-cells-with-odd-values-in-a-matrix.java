b5562393b2c09a34c3f89088cc22c622ef014b73
Rohan Shirke <96473300+rohanshirke00@users.noreply.github.com>
2022-07-31 20:47:24 +0530

    Time: 1 ms (89.64%), Space: 42.3 MB (35.32%) - LeetHub

--- 1252-cells-with-odd-values-in-a-matrix/1252-cells-with-odd-values-in-a-matrix.java
+++ 1252-cells-with-odd-values-in-a-matrix/1252-cells-with-odd-values-in-a-matrix.java
@@ -1,24 +1,52 @@
-class Solution {
-    public int oddCells(int m, int n, int[][] indices) {
-        int [][] ans = new int[m][n];
-        for(int i=0; i<indices.length; i++){
-            for(int j=0; j<n; j++){
-                ans[indices[i][0]][j]++;
-            }
-            for(int j=0; j<m; j++){
-                ans[j][indices[i][1]]++;
-            }
+// class Solution {
+//     public int oddCells(int m, int n, int[][] indices) {
+//         int [][] ans = new int[m][n];
+//         for(int i=0; i<indices.length; i++){
+//             for(int j=0; j<n; j++){
+//                 ans[indices[i][0]][j]++;
+//             }
+//             for(int j=0; j<m; j++){
+//                 ans[j][indices[i][1]]++;
+//             }
             
-        }
+//         }
         
-        int count = 0;
+//         int count = 0;
         
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                if(!(ans[i][j]%2==0)) count ++;
-            }
-        }
-        return count;
+//         for(int i=0; i<m; i++){
+//             for(int j=0; j<n; j++){
+//                 if(!(ans[i][j]%2==0)) count ++;
+//             }
+//         }
+//         return count;
+        
+//     }
+// }
+
+
+class Solution {
+public int oddCells(int m, int n, int[][] indices) {
+int[] r=new int[m];
+int[] c=new int[n];
+
+    int len=indices.length;
+    for(int i=0;i<len;i++){
+        r[indices[i][0]]++;
+        c[indices[i][1]]++;
+        
         
     }
+    int[][] arr=new int[m][n];
+    int count=0;
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            arr[i][j]=r[i]+c[j];
+            if(arr[i][j]%2!=0){
+                count++;
+            }
+        }
+    }
+   
+    return count;
+}
 }
