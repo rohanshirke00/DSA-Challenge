5418f323b468b4d17162a0dcffa6400ba1e14634
Rohan Shirke <96473300+rohanshirke00@users.noreply.github.com>
2022-08-06 14:39:05 +0530

    Time: 1 ms (81.72%), Space: 42.3 MB (48.55%) - LeetHub

--- 1854-maximum-population-year/1854-maximum-population-year.java
+++ 1854-maximum-population-year/1854-maximum-population-year.java
@@ -1,3 +1,45 @@
+// class Solution {
+//     public int maximumPopulation(int[][] logs) {
+//         int[] num = new int[101];   //1950-250 =100 yr
+        
+//         //travering the logs[][] array
+//         for(int i=0; i<logs.length; i++){   
+            
+//             //incrementing the yr from x year to y year
+//             for(int j=logs[i][0]; j<logs[i][1]; j++){
+                
+//                 //incrementing the yr at index
+//                 num[j - 1950]++;
+                
+//             }
+//         }
+        
+//         int maxValue=0;
+//         int maxYear=1950;
+        
+//         //checking which yr has maximum count
+//         for(int i=0; i<101; i++){
+//             if(num[i]>maxValue){
+//                 maxValue=num[i];
+//                 maxYear = i+1950;
+                
+//             }
+//         }
+        
+//         return maxYear;
+        
+//         1950-1950   - 0+1
+        
+//     }
+// }
+
+
+
+
+
+
+
+
 class Solution {
     public int maximumPopulation(int[][] logs) {
         int[] num = new int[101];   //1950-250 =100 yr
@@ -5,15 +47,17 @@ class Solution {
         //travering the logs[][] array
         for(int i=0; i<logs.length; i++){   
             
-            //incrementing the yr from x year to y year
-            for(int j=logs[i][0]; j<logs[i][1]; j++){
-                
-                //incrementing the yr at index
-                num[j - 1950]++;
-                
-            }
+            //incrementing the particular Birth year & decrementing the death year
+			
+			num[logs[i][0]  - 1950]++;
+			num[logs[i][1]  - 1950]--;
         }
         
+		//calculating the running sum
+		for(int i=1; i<101; i++){
+			num[i] = num[i] + num[i-1];
+		}
+		
         int maxValue=0;
         int maxYear=1950;
         
@@ -22,7 +66,7 @@ class Solution {
             if(num[i]>maxValue){
                 maxValue=num[i];
                 maxYear = i+1950;
-                
+               
             }
         }
         
