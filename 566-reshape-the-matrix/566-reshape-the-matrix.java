b86e472767bb9aaf3398769679462b1e49ab8353
Rohan Shirke <96473300+rohanshirke00@users.noreply.github.com>
2022-08-09 16:32:26 +0530

    Time: 3 ms (10.60%), Space: 51 MB (24.30%) - LeetHub

---
+++ 566-reshape-the-matrix/566-reshape-the-matrix.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[][] matrixReshape(int[][] mat, int r, int c) {
+        if(mat.length*mat[0].length != r*c){
+            return mat;
+        }
+        int[][] arr = new int [r][c];
+        List<Integer> list = new ArrayList<>();
+        
+        for(int i=0; i<mat.length; i++){
+            for(int j=0; j<mat[i].length; j++){
+                list.add(mat[i][j]);
+            }
+        }
+        
+        int k=0;
+        for(int i=0; i<r; i++){
+            for(int j=0; j<c; j++){
+                arr[i][j] = list.get(k);
+                k++;
+            }
+        }
+        
+        return arr;
+    }
+}
