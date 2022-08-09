9b65f95ada8e859d5fcc56e0ec72c291e2babfa3
Rohan Shirke <96473300+rohanshirke00@users.noreply.github.com>
2022-08-09 16:12:05 +0530

    Time: 3 ms (25.85%), Space: 73.4 MB (68.19%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.java
+++ 53-maximum-subarray/53-maximum-subarray.java
@@ -1,51 +1,51 @@
 
 //DP approach
 
-class Solution {
-    public int maxSubArray(int[] nums) {
+// class Solution {
+//     public int maxSubArray(int[] nums) {
         
-        // Initialize currMaxSum & take first element of array from which we start to do sum...
-        int maxSum = nums[0];
+//         // Initialize currMaxSum & take first element of array from which we start to do sum...
+//         int maxSum = nums[0];
         
-        // Initialize the current sum of our subarray as nums[0]...
-        int currSum = nums[0];
+//         // Initialize the current sum of our subarray as nums[0]...
+//         int currSum = nums[0];
         
-        // Traverse all the element through the loop...
-        for (int i = 1; i < nums.length; i++) {
-            // Do sum of elements contigous with curr sum...
-            // Compare it with array element to get maximum result...
-            currSum = Math.max(currSum + nums[i], nums[i]);
+//         // Traverse all the element through the loop...
+//         for (int i = 1; i < nums.length; i++) {
+//             // Do sum of elements contigous with curr sum...
+//             // Compare it with array element to get maximum result...
+//             currSum = Math.max(currSum + nums[i], nums[i]);
             
-            // Compare current sum and max sum.
-            maxSum = Math.max(maxSum, currSum);
-        }
-        return maxSum;      // return the contiguous subarray which has the largest sum...
-    }
-}
+//             // Compare current sum and max sum.
+//             maxSum = Math.max(maxSum, currSum);
+//         }
+//         return maxSum;      // return the contiguous subarray which has the largest sum...
+//     }
+// }
 
 
 
 // Kadanes approach
 
-// class Solution {
-//     public int maxSubArray(int[] nums) {
+class Solution {
+    public int maxSubArray(int[] nums) {
         
-//         int n = nums.length;
-//         int currentSum = 0;
-//         int max=Integer.MIN_VALUE;
+        int n = nums.length;
+        int currentSum = 0;
+        int max=Integer.MIN_VALUE;
         
         
-//         for(int i=0; i<n; i++){
+        for(int i=0; i<n; i++){
             
-//             currentSum = currentSum + nums[i];
+            currentSum = currentSum + nums[i];
             
-//             max = currentSum>max?currentSum:max;
+            max = currentSum>max?currentSum:max;
             
-//             if(currentSum < 0){
-//                 currentSum = 0;
-//             }
-//         }
+            if(currentSum < 0){
+                currentSum = 0;
+            }
+        }
         
-//         return max;
-//     }
-// }
+        return max;
+    }
+}
