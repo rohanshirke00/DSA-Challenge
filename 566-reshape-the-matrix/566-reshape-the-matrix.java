5bb6ac9c21ff0879133580b86b67484f7960b936
Rohan Shirke <96473300+rohanshirke00@users.noreply.github.com>
2022-08-09 19:08:32 +0530

    Time: 1 ms (90.08%), Space: 43.2 MB (94.00%) - LeetHub

--- 566-reshape-the-matrix/566-reshape-the-matrix.java
+++ 566-reshape-the-matrix/566-reshape-the-matrix.java
@@ -1,23 +1,41 @@
+// class Solution {
+//     public int[][] matrixReshape(int[][] mat, int r, int c) {
+//         if(mat.length*mat[0].length != r*c){
+//             return mat;
+//         }
+//         int[][] arr = new int [r][c];
+//         List<Integer> list = new ArrayList<>();
+        
+//         for(int i=0; i<mat.length; i++){
+//             for(int j=0; j<mat[i].length; j++){
+//                 list.add(mat[i][j]);
+//             }
+//         }
+        
+//         int k=0;
+//         for(int i=0; i<r; i++){
+//             for(int j=0; j<c; j++){
+//                 arr[i][j] = list.get(k);
+//                 k++;
+//             }
+//         }
+        
+//         return arr;
+//     }
+// }
+
+
+
+
 class Solution {
     public int[][] matrixReshape(int[][] mat, int r, int c) {
         if(mat.length*mat[0].length != r*c){
             return mat;
         }
         int[][] arr = new int [r][c];
-        List<Integer> list = new ArrayList<>();
         
-        for(int i=0; i<mat.length; i++){
-            for(int j=0; j<mat[i].length; j++){
-                list.add(mat[i][j]);
-            }
-        }
-        
-        int k=0;
-        for(int i=0; i<r; i++){
-            for(int j=0; j<c; j++){
-                arr[i][j] = list.get(k);
-                k++;
-            }
+        for(int i=0; i<r*c; i++){
+            arr[i/c][i%c] = mat[i/mat[0].length][i%mat[0].length];
         }
         
         return arr;
