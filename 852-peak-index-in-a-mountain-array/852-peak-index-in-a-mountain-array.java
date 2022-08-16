0b0110b6f6d726304d2bee336360c5745f159032
Rohan Shirke <96473300+rohanshirke00@users.noreply.github.com>
2022-08-16 22:13:18 +0530

    Time: 0 ms (100.00%), Space: 59.1 MB (62.21%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.java
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.java
@@ -5,31 +5,16 @@ class Solution {
         
         int s = 0;
         int e = arr.length-1;
-        int ans=-1;
+
         while(s<e){
             int mid = s + (e-s) / 2;   
             if(arr[mid] < arr[mid+1]){
-                ans = mid+1;
                 s = mid+1;
             }
             else{
-                ans = mid;
                 e = mid;
             }
         }   
-        return ans;
+        return s; // return e
     }
 }
-
-
-
-
-//linear search O(N)
-
-// class Solution {
-//     public int peakIndexInMountainArray(int[] arr) {
-//         int i=0;
-//         while(arr[i]<arr[i+1]) i++;
-//         return i;       
-//     }
-// }
