a252536741680ba7d48f18147376d4ecd997e35c
Rohan Shirke <96473300+rohanshirke00@users.noreply.github.com>
2022-08-16 11:23:14 +0530

    Time: 5 ms (41.45%), Space: 41 MB (79.66%) - LeetHub

--- 441-arranging-coins/441-arranging-coins.java
+++ 441-arranging-coins/441-arranging-coins.java
@@ -3,20 +3,42 @@ class Solution {
         long start = 0;
         long end = n;
 
+//         while(start<=end){
+//             long mid = start + (end-start)/2;
+            
+//             if((mid*(mid+1)/2) <= n){
+                
+//                 start = mid+1;
+                
+//             }
+//             else{
+                
+//                 end = mid-1;
+            
+//             }
+//         }
+//         return (int)(start)-1;
+        
+        
+        
         while(start<=end){
             long mid = start + (end-start)/2;
             
-            if((mid*(mid+1)/2) <= n){
+            if((mid*(mid+1)/2) == n){
+                
+                return (int)mid;
+                
+            }
+            else if((mid*(mid+1)/2) <= n){
                 
                 start = mid+1;
-                
-            }
-            else{
-                
-                end = mid-1;
             
+            }else{
+                end = mid-1;
             }
         }
-        return (int)(start)-1;
+
+        return (int)end;
+
     }
 }
