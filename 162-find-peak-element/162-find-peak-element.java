346b4b2b2c6747c2b2461929ae20d9bff3ccbf4f
Rohan Shirke <96473300+rohanshirke00@users.noreply.github.com>
2022-08-16 22:15:20 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (83.08%) - LeetHub

---
+++ 162-find-peak-element/162-find-peak-element.java
@@ -0,0 +1,20 @@
+//Binary search O(LogN)
+
+class Solution {
+    public int findPeakElement(int[] arr) {
+        
+        int s = 0;
+        int e = arr.length-1;
+
+        while(s<e){
+            int mid = s + (e-s) / 2;   
+            if(arr[mid] < arr[mid+1]){
+                s = mid+1;
+            }
+            else{
+                e = mid;
+            }
+        }   
+        return s; // return e
+    }
+}
