f6d9b4ff2cf701f5c81d05964ad56b06ccd5e8b1
Rohan Shirke <96473300+rohanshirke00@users.noreply.github.com>
2022-08-16 22:52:42 +0530

    Time: 0 ms (100.00%), Space: 43.5 MB (75.00%) - LeetHub

---
+++ 1095-find-in-mountain-array/1095-find-in-mountain-array.java
@@ -0,0 +1,79 @@
+/**
+ * // This is MountainArray's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * interface MountainArray {
+ *     public int get(int index) {}
+ *     public int length() {}
+ * }
+ */
+ 
+class Solution {
+    public int findInMountainArray(int target, MountainArray mountainArr) {
+        
+        int start = 0;
+        int end = mountainArr.length()-1;
+        int peakIndex = peakIndexInMountainArray(mountainArr);
+        
+        int firstHalf = orderAgnosticBinarySearch(mountainArr,target,start,peakIndex);
+        
+        if(firstHalf != -1){
+            return firstHalf;
+        }
+        return orderAgnosticBinarySearch(mountainArr,target,peakIndex,end);
+    }
+    
+    
+    
+    //Binary search O(LogN)
+
+    public int peakIndexInMountainArray(MountainArray mountainArr) {
+        
+        int s = 0;
+        int e = mountainArr.length()-1;
+
+        while(s<e){
+            int mid = s + (e-s) / 2;   
+            if(mountainArr.get(mid) < mountainArr.get(mid+1)){
+                s = mid+1;
+            }
+            else{
+                e = mid;
+            }
+        }   
+        return s; // return e
+    }
+
+    
+    
+    static int orderAgnosticBinarySearch(MountainArray mountainArr, int target, int start, int end){
+        
+        if(mountainArr.get(start) < mountainArr.get(end)){
+            while(start<=end){
+                int mid = start + (end-start)/2;
+                if(target==mountainArr.get(mid)){
+                    return mid;
+                }else if(target < mountainArr.get(mid)){
+                    end = mid - 1;
+                }else{
+                    start = mid+1;
+                }
+            }
+            return -1;
+        }else{
+            
+            while(start<=end){
+                int mid = start + (end-start)/2;
+                if(target==mountainArr.get(mid)){
+                    return mid;
+                }else if(target > mountainArr.get(mid)){
+                    end = mid - 1;
+                }else{
+                    start = mid+1;
+                }
+            }
+            return -1;
+        }
+        
+    }
+    
+}
