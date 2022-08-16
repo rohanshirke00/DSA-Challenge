10945d5378b7df9ef2c4a305d5b88e9958645278
Rohan Shirke <96473300+rohanshirke00@users.noreply.github.com>
2022-08-16 12:29:52 +0530

    Time: 0 ms (100.00%), Space: 42.9 MB (74.71%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
@@ -1,9 +1,35 @@
+
+// linear search O(N)
+
+// class Solution {
+//     public int findKthPositive(int[] arr, int k) {
+//         for(int i=0; i<arr.length; i++){
+//             if(k>=arr[i]) k++;
+//             else break;
+//         }
+//         return k;
+//     }
+// }
+
+
+//Binary search O(logN)
+
 class Solution {
     public int findKthPositive(int[] arr, int k) {
-        for(int i=0; i<arr.length; i++){
-            if(k>=arr[i]) k++;
-            else break;
+ 
+        int s = 0;
+        int e = arr.length-1;
+        
+        while(s<=e){
+            int mid = s + (e-s) / 2;
+            
+            if(arr[mid] - (mid+1) >= k){
+                e = mid-1;
+            }else{
+                s = mid+1;
+            }
+            
         }
-        return k;
+        return s+k;
     }
 }
