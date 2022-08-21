3440b94b7dd79157758aa3257fdfb3c4f445eab6
Rohan Shirke <96473300+rohanshirke00@users.noreply.github.com>
2022-08-21 23:50:53 +0530

    Time: 5 ms (23.33%), Space: 43.3 MB (64.82%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.java
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.java
@@ -1,13 +1,54 @@
+// class Solution {
+//     public boolean checkIfExist(int[] arr) {
+        
+//         Set<Integer> set = new HashSet<>();
+//         for(int a : arr){
+//             if(set.contains(a*2) || (a%2 == 0 && set.contains(a/2)))
+//                 return true;
+//             set.add(a);
+//         }
+//         return false;
+//     }
+    
+// }
+
+
+
+
+//binary search
+
 class Solution {
     public boolean checkIfExist(int[] arr) {
         
-        Set<Integer> set = new HashSet<>();
-        for(int a : arr){
-            if(set.contains(a*2) || (a%2 == 0 && set.contains(a/2)))
+        Arrays.sort(arr);
+        
+        for(int i = 0; i<arr.length; i++){
+            
+            int res = binarySearch(arr, arr[i]*2);
+            if( res != i && res != -1){
                 return true;
-            set.add(a);
+            }
+            
         }
+        
         return false;
+    }
+    
+    static int binarySearch(int[] arr, int target){
+        
+        int s=0;
+        int e=arr.length-1;
+        while(s<=e){
+            int mid = s + (e-s)/2;
+            if(arr[mid]==target){
+                return mid;
+            }else if(arr[mid]<target){
+                s = mid+1;
+            }else{
+                e = mid-1;
+            }
+        }
+        return -1;
     }
     
 }
