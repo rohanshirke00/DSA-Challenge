1cf01d1fd04b4fbd443a5262e5dfee5009e26714
Rohan Shirke <96473300+rohanshirke00@users.noreply.github.com>
2022-08-21 20:35:40 +0530

    Time: 29 ms (39.95%), Space: 72.7 MB (14.02%) - LeetHub

--- 888-fair-candy-swap/888-fair-candy-swap.java
+++ 888-fair-candy-swap/888-fair-candy-swap.java
@@ -1,15 +1,17 @@
-
 class Solution {
-  public int[] fairCandySwap(int[] A, int[] B) {
-    int diff = (IntStream.of(A).sum() - IntStream.of(B).sum()) / 2;
-    Set<Integer> set = new HashSet<>();
-    for (int b : B)
-      set.add(b);
 
-    for (int a : A)
-      if (set.contains(a - diff))
-        return new int[] {a, a - diff};
+    public int[] fairCandySwap(int[] A, int[] B) {
+    
+        int diff = (IntStream.of(A).sum() - IntStream.of(B).sum()) / 2;
+        Set<Integer> set = new HashSet<>();
+        for (int b : B)
+          set.add(b);
+    
+        for (int a : A)
+          if (set.contains(a - diff))
+            return new int[] {a, a - diff};
 
-    throw new IllegalArgumentException();
-  }
+        return null;
+    }
+
 }
