f133e12dfa23fe2ab5ea3e800732a92c4dccdbae
Rohan Shirke <96473300+rohanshirke00@users.noreply.github.com>
2022-08-25 21:03:16 +0530

    Time: 1 ms (92.44%), Space: 40.9 MB (84.84%) - LeetHub

---
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        
+        List<Integer> list = new ArrayList<>();
+        
+        long val = 1;
+        for(int j=0; j<= rowIndex; j++){
+            list.add((int)val);
+            val = (val * (rowIndex-j)) / (j+1);
+        }
+        
+        return list;
+    }
+}
