864c475235f7abc1319257f70966602d04f80a97
Rohan Shirke <96473300+rohanshirke00@users.noreply.github.com>
2022-08-27 12:45:17 +0530

    Time: 1 ms (100.00%), Space: 51.5 MB (95.03%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.java
+++ 53-maximum-subarray/53-maximum-subarray.java
@@ -1,28 +1,28 @@
+// class Solution{
+    
+//     public static int maxSubArray(int[] arr) {
 
-//DP approach
+//         int currentBest = 0;
+//         int overAllBest = arr[0];
 
-// class Solution {
-//     public int maxSubArray(int[] nums) {
-        
-//         // Initialize currMaxSum & take first element of array from which we start to do sum...
-//         int maxSum = nums[0];
-        
-//         // Initialize the current sum of our subarray as nums[0]...
-//         int currSum = nums[0];
-        
-//         // Traverse all the element through the loop...
-//         for (int i = 1; i < nums.length; i++) {
-//             // Do sum of elements contigous with curr sum...
-//             // Compare it with array element to get maximum result...
-//             currSum = Math.max(currSum + nums[i], nums[i]);
-            
-//             // Compare current sum and max sum.
-//             maxSum = Math.max(maxSum, currSum);
+//         for(int i=1; i< arr.length; i++){
+
+//             if(currentBest >= 0){
+//                 currentBest += arr[i];
+//             }
+//             else{
+//                 currentBest = arr[i];
+//             }
+
+//             if(currentBest > overAllBest){
+//                 overAllBest = currentBest;
+//             }
+
 //         }
-//         return maxSum;      // return the contiguous subarray which has the largest sum...
+
+//         return overAllBest;
 //     }
 // }
-
 
 
 // Kadanes approach
