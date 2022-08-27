62a9234484d70d1edbb59311cdeefc01dedbdefc
Rohan Shirke <96473300+rohanshirke00@users.noreply.github.com>
2022-08-27 12:47:27 +0530

    Time: 1 ms (100.00%), Space: 73.9 MB (49.08%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.java
+++ 53-maximum-subarray/53-maximum-subarray.java
@@ -1,51 +1,51 @@
-// class Solution{
+class Solution{
     
-//     public static int maxSubArray(int[] arr) {
+    public static int maxSubArray(int[] arr) {
 
-//         int currentBest = 0;
-//         int overAllBest = arr[0];
+        int currentBest = arr[0];
+        int overAllBest = arr[0];
 
-//         for(int i=1; i< arr.length; i++){
+        for(int i=1; i< arr.length; i++){
 
-//             if(currentBest >= 0){
-//                 currentBest += arr[i];
-//             }
-//             else{
-//                 currentBest = arr[i];
-//             }
+            if(currentBest >= 0){
+                currentBest += arr[i];
+            }
+            else{
+                currentBest = arr[i];
+            }
 
-//             if(currentBest > overAllBest){
-//                 overAllBest = currentBest;
-//             }
+            if(currentBest > overAllBest){
+                overAllBest = currentBest;
+            }
 
-//         }
+        }
 
-//         return overAllBest;
-//     }
-// }
+        return overAllBest;
+    }
+}
 
 
 // Kadanes approach
 
-class Solution {
-    public int maxSubArray(int[] nums) {
+// class Solution {
+//     public int maxSubArray(int[] nums) {
         
-        int n = nums.length;
-        int currentSum = 0;
-        int max=Integer.MIN_VALUE;
+//         int n = nums.length;
+//         int currentSum = 0;
+//         int max=Integer.MIN_VALUE;
         
         
-        for(int i=0; i<n; i++){
+//         for(int i=0; i<n; i++){
             
-            currentSum = currentSum + nums[i];
+//             currentSum = currentSum + nums[i];
             
-            max = currentSum>max?currentSum:max;
+//             max = currentSum>max?currentSum:max;
             
-            if(currentSum < 0){
-                currentSum = 0;
-            }
-        }
+//             if(currentSum < 0){
+//                 currentSum = 0;
+//             }
+//         }
         
-        return max;
-    }
-}
+//         return max;
+//     }
+// }
