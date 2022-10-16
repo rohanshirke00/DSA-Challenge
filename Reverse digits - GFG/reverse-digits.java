a9439c2b71b4270618fc717fa5b257999c20f031
Rohan Shirke <96473300+rohanshirke00@users.noreply.github.com>
2022-10-16 21:31:22 +0530

    Added solution - LeetHub

---
+++ Reverse digits - GFG/reverse-digits.java
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            long n = Long.parseLong(br.readLine().trim());            
+            Solution ob = new Solution();
+            long ans = ob.reverse_digit(n);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public long reverse_digit(long n)
+    {
+        String reverse="";
+        while(n!=0)
+        {
+            long r=n%10;
+            n=n/10;
+            reverse+=Long.toString(r);
+        }
+        long ans=Long.parseLong(reverse);
+        
+        return ans;
+    }
+}
